dbdd3e3fa67515d793ebbf14ea3f2152ecc2ee20
shawinder706 <shawinder706>
2019-11-23 14:03:34 +0530

    second commit

--- first-node-app/index.js
+++ first-node-app/index.js
@@ -4,6 +4,9 @@
 
   First Sure that system has the node installed  
 
+  // initial commit 
+  *****************************
+
   1. Create new deirectory and open in the command prompt window.
 
   2. Type the 'npm init' command and press enter. It will create the package.json file
@@ -13,18 +16,29 @@
 
   4. Run index.js file by typing 'node index.js' in the cmd window.                 
 
+ // second commit
+ ********************************* 
+
+  1. create the rectangle.js
+
+  2. create new method in the rectangle.js files
+
+  3. comment the var rect object
 
 */
 
+var rect=require('./rectangle');
 
-var rect={
-    perimeter:(x,y)=>(2*(x+y)),
-    area:(x,y)=>(x*y)
-};
+// var rect={
+//     perimeter:(x,y)=>(2*(x+y)),
+//     area:(x,y)=>(x*y)
+// };
+
+
 function solveRect(l,b){
     console.log(`solving for rectangle with l=${l} and b=${b} `);
     if(l<=0 || b<=0){
-        console.log(`Rectangle dimension should be greater thanm zero: l=${l} and b=${b}`);
+        console.log(`Rectangle dimension should be greater than zero:: l=${l} and b=${b}`);
     }
     else{
         console.log(`The area of the rectangle is ${rect.area(l,b)}`);
---
+++ first-node-app/rectangle.js
@@ -0,0 +1,6 @@
+
+// 1.Export the mehods using exports 
+// 2. require these methods in index.js for using in the index.js
+
+exports.perimeter=(x,y)=>(2*(x+y));
+exports.area=(x,y)=>(x*y);
