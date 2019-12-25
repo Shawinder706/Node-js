33d8d5025f9cafc21714ce787c8facae9b4e0179
Shawinder706 <shawinder771@gmail.com>
2019-12-25 15:12:45 +0530

    mongoose part 1

--- Node-Mongoose/index.js
+++ Node-Mongoose/index.js
@@ -7,36 +7,21 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
     
-    Dishes.create({
-        name: 'Ut',
-        description:"welcome Uthappizza",        
-    })
-    .then((dish) => {
-        console.log(dish);
-        return Dishes.findByIdAndUpdate(dish._id,{
-            $set:{description:"updated test"}
-        },{
-            new:true
+    var newDish = Dishes({
+        name : 'shawinder singh',
+        description:'test'
+
+    });
+    newDish.save()
+        .then((dish)=>{
+            console.log(`save ${dish}`)
+           // return Dishes.find({});
+        });
+        .then((dishes)=>{
+            console.log(`remove ${dishes}`)
+            return Dishes.remove({});
         })
-        .exec();
-    })
-    .then((dish) => {
-        console.log(dish);
-        dish.comments.push({
-            rating:5,
-            comment: 'I\ m gettting a sinking feeling !',
-            author:'leonardo di carpaccio'
-        })
-    })
-    .then(() => {
-        return mongoose.connection.close();
-    })
-        return dish.save()
-})
-.then((dish) =>{
-    console.log(dish)
-    return Dishes.remove({ });
-})
-.catch((err)=>{
-    console.log(err)
+        .then(()=>{
+            return mongoose.connection.close();
+        });
 });
