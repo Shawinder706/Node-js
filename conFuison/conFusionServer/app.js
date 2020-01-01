9876f784f5ac6c12be2bb35743829dc89258b4d3
Shawinder706 <shawinder771@gmail.com>
2020-01-01 22:28:23 +0530

    Basic Authentication

--- conFuison/conFusionServer/app.js
+++ conFuison/conFusionServer/app.js
@@ -42,54 +42,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //app.use(cookieParser());
 
-
-// basic authentication 
-
-app.use(session({
-  name:'session-id',
-  secret:'12345-6789-09876-54321',
-  saveUninitialized:false,
-  resave:false,
-  store:new FileStore()
-
-}));
-
 function auth(req,res,next){
-  console.log(req.session)
-  if(!req.session.user){
-    var authHeader=req.headers.authorization
-    if(!authHeader){
-      var err=new Error("you are not authenticated!");
-      res.setHeader('www-Authenticate','Basic')
-      err.status=401,
-      next(err)
-      return
-    }
-
-    var auth=new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':')
-    var user=auth[0]
-    var pass=auth[1]
-
-    if(user=='user' && pass=='password'){
-      req.session.user='user';
-      next();
-    }else{
-      var err = new Error('you are not autheticated!')
-      res.setHeader('www-Authenticate','Basic')
-      err.status=401,
-      next(err)
-    }
+  console.log(req.headers);
+  var authHeader=req.headers.authorization;
+  if(!authHeader){
+    var err=new Error("You are not authenticated !");
+    res.setHeader('WWW-Authenticate','Basic');
+    err.status=401
+    next(err)
+    return
   }
-  else{
-    if(req.session.user==='admin'){
-      console.log('req.session:',req.session)
-      next(err)
-    }
-  } 
-
+  var auth=new Buffer.from(authHeader.split('')[1],'base64').toString().split(':')
+  var user=auth[0];
+  var pass=auth[1];
+  if(user=='admin' && pass=='password'){
+    next()
+  }else{
+      var err=new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status=401
+      next(err);
+  }
 }
 
 app.use(auth)
+
 app.use(express.static(path.join(__dirname, 'public/html/')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
